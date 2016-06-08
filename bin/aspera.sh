53b7b33232abad35f4f92add8819528e17215b59
Andrew Magill <andrewbmagill@yahoo.com>
2016-06-08 11:18:39 -0500

    some typo fixes, and tons of writing to the console.

--- bin/aspera.sh
+++ bin/aspera.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+echo
+echo "Begin debug messages from aspera.sh"
+echo
+
 # Set constant variables
 ASPERA="~/.aspera/connect"
 SRA=$1
 DESTINATION_DIR=$2
 
+echo "INPUT"
+echo
+echo "SRA: ${SRA}"
+echo "DESTINATION_DIR: ${DESTINATION_DIR}"
+echo
+
 if [ "${DESTINATION_DIR: -1}" != '/' ]; then
 	echo 'Destination directory is not valid!'
+
+	echo
+	echo "End debug messages from aspera.sh"
+	echo
+
 	exit 1
 fi
 
@@ -16,9 +31,15 @@ fi
 PREFIX=`echo $SRA | cut -b 1-3`
 PREFIX_ACCESSION=`echo $SRA | cut -b 1-6`
 
+echo "Get the first 6 characters of SRA accession."
+echo "PREFIX: ${PREFIX}"
+echo "PREFIX_ACCESSION: ${PREFIX_ACCESSION}"
+echo
+
 # Join command
 FTP_PREFIX='anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra'
 FTP_ADDRESS="$FTP_PREFIX/$PREFIX/$PREFIX_ACCESSION/$SRA/$SRA.sra"
+
 echo -e "ftp address:\n$FTP_ADDRESS"
 
 # Create a new directory if not exists
@@ -29,12 +50,25 @@ else
 	echo "Destination is: $DESTINATION_DIR"
 fi
 
-ascp -T -l640M -i ~/.aspera/connect/etc/asperaweb_id_dsa.openssh $FTP_ADDRESS $DESTINATION_DIR
+echo "Download using ascp."
+echo "$ ~/.aspera/connect/bin/ascp -T -l640M -i ~/.aspera/connect/etc/asperaweb_id_dsa.openssh $FTP_ADDRESS $DESTINATION_DIR"
+
+~/.aspera/connect/bin/ascp -T -l640M -i ~/.aspera/connect/etc/asperaweb_id_dsa.openssh $FTP_ADDRESS $DESTINATION_DIR
 
 if [ $? -eq 0 ]; then
 	echo 'Download successful!'
+
+	echo
+	echo "End debug messages from aspera.sh"
+	echo
+
 	exit 0
 else
 	echo 'Download Fail!'
-	exit 1	
+
+	echo
+	echo "End debug messages from aspera.sh"
+	echo
+
+	exit 1
 fi
--- bin/install.sh
+++ bin/install.sh
@@ -1,22 +1,56 @@
 #!/bin/bash
 
+echo "Install wget and unzip."
+echo "$ apt-get install -y wget unzip"
+echo
+echo "apt-get exit code: ${?}"
+echo
+
 # get the tool of wget and unzip
-apt-get install -y wget unzip 
+apt-get install -y wget unzip
 
 # download sratoolkit
-# For format convertion? 
+# For format convertion?
 # echo "Downloading sratoolkit from ncbi..."
 # wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.5.7/sratoolkit.2.5.7-ubuntu64.tar.gz
 # tar xvzf sratoolkit.2.5.7-ubuntu64.tar.gz
 # chmod a+x sratoolkit.2.5.7-ubuntu64/bin/*
 
+echo
+echo "Begin debug messages from install.sh"
+echo
+
 # download aspera connect
-echo "Downloading Aspera Connect tool..."
+echo "Download Aspera Connect tool."
+echo "$ wget http://download.asperasoft.com/download/sw/connect/3.6.2/aspera-connect-3.6.2.117442-linux-64.tar.gz"
+
 wget http://download.asperasoft.com/download/sw/connect/3.6.2/aspera-connect-3.6.2.117442-linux-64.tar.gz
+
+echo
+echo "wget exit code: ${?}"
+echo
+
+echo "Uncompress downloaded file."
+echo "$ tar xvzf aspera-connect-3.6.2.117442-linux-64.tar.gz"
+
 tar xvzf aspera-connect-3.6.2.117442-linux-64.tar.gz
+
+echo "Install aspera."
+echo "$ ./aspera-connect-3.6.2.117442-linux-64.sh"
+
 ./aspera-connect-3.6.2.117442-linux-64.sh
-chmod a+x ~/.aspera/connect/bin/* 
+
+echo
+echo "install exit code: ${?}"
+echo
+
+echo "Make contents of bin executable."
+echo "$ chmod a+x ~/.aspera/connect/bin/*"
+
+chmod a+x ~/.aspera/connect/bin/*
 # export PATH=$PATH:"$HOME/.aspera/connect/bin"
 # echo "PATH=\$PATH:\$HOME/.aspera/connect/bin" >> ~/.bashrc
 
-
+echo
+echo "End debug messages from install.sh"
+echo
